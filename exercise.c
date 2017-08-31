2b896b3aa6da0b2799417804298d31c8a16f366d
Rafid <rafidbinsaad@gmail.com>
2017-08-31 00:00:23 -0400

    1.20 bug

--- exercise.c
+++ exercise.c
@@ -8,18 +8,22 @@ char longest[MAXLINE]; /* maximum length seen so far */
 /* longest line saved here */
 int getlines(void);
 void copy(void);
+
+int n = 1; /* tab size in spaces */
+
 /* print longest input line; specialized version */
 int main() {
   int len;
   extern int max;
   extern char longest[];
   max = 0;
-  while ((len = getlines()) > 0)
+  while ((len = getlines()) > 0) {
+    printf("This is the line: %s", line);
     if (len > max) {
       max = len;
       copy();
     }
-
+  }
   if (max > 0) /* there was a line */
     printf("%s", longest);
   return 0;
@@ -27,16 +31,29 @@ int main() {
 
 /* getlines: specialized version */
 int getlines(void) {
-  int c, i;
+  int c, i, k;
+  extern int n;
   extern char line[];
-  for (i = 0; i < (MAXLINE - 1) && (c = getchar()) != EOF && c != '\n'; ++i)
-    line[i] = c;
+  for (i = 0; i < (MAXLINE - 1) && (c = getchar()) != EOF && c != '\n'; ++i) {
+    if (c == '\t') {
+      for (k = 0;  k < n;  ++k) {
+        printf("For tab comes here %d\n", k);
+        ++i;
+        line[i] = ' ';
+      }
+    } else {
+      line[i] = c;
+    }
+      printf("value of i %d and characger is %d\n", i, line[i]);
+      putchar(c);
+    }
 
   if (c == '\n') {
     line[i] = c;
     ++i;
   }
   line[i] = '\0';
+  printf("length is %d\n", i);
   return i;
 }
 
